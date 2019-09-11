a330df020a58599009aaee37781d0f6bfcaeb558
amiceli <antoine@miceli.click>
2019-09-11 23:05:17 +0200

    Add 4 spaces inside switch case.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
     rules : {
         'no-console' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        indent : ['error', 4],
+        indent : ['error', 4, { "SwitchCase": 1 }],
         semi : ['error', 'never'],
         'space-before-function-paren' : ['error', 'always'],
         'import/extensions' : 'off',
