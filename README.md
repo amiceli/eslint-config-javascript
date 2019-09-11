80b7ac32dbc483499329d1cdac16017169e087bc
amiceli <antoine@miceli.click>
2019-09-11 22:29:56 +0200

    Create repository

---
+++ README.md
@@ -0,0 +1,21 @@
+# eslint-config-javascript
+
+## Install
+
+With npm :
+
+    npm i @amiceli/eslint-config-javascript -S
+
+## Configuration
+
+Create eslint config file : `.eslintrc.js`
+
+And add following code :
+
+~~~js
+module.exports = {
+    extends : [
+        '@amiceli/eslint-config-javascript'
+    ]
+}
+~~~
---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root : true,
+    env : {
+        node : true
+    },
+    extends : [
+        'airbnb'
+    ],
+    rules : {
+        'no-console' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent : ['error', 4],
+        semi : ['error', 'never'],
+        'space-before-function-paren' : ['error', 'always'],
+        'import/extensions' : 'off',
+        'class-methods-use-this' : 0,
+        'no-param-reassign' : 'off',
+        'max-len' : ['error', {code : 200}],
+        'padded-blocks' : ['error', {classes : 'always'}],
+        'key-spacing' : ['error', {afterColon : true, beforeColon : true}],
+        'import/no-extraneous-dependencies' : 'off',
+        'import/prefer-default-export' : 'off'
+    }
+}
